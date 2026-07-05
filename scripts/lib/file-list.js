ccef8e8a624dc678bd9babeda2c6a9c3a6e4a9fc
Root <243567670+astro-root@users.noreply.github.com>
2026-07-05 04:05:28 +0000

    feat: header/footerをpartials化し、同期スクリプトを導入(index.html, about.htmlに適用)

---
+++ scripts/lib/file-list.js
@@ -0,0 +1,14 @@
+module.exports = [
+  "index.html",
+  "about.html",
+  "contact.html",
+  "contact-success.html",
+  "privacy.html",
+  "404.html",
+  "astronomy/index.html",
+  "blog/index.html",
+  "lab/index.html",
+  "projects/index.html",
+  "research-log/index.html",
+  "study/index.html"
+];
